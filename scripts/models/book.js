99b6660b70dddde22b7db9295de03a95dfef7662
Michael Sklepowich <michaelsklepowich@Michaels-MBP-3.localdomain>
2018-04-25 11:35:23 -0700

    adding modified Article.js constructor into Book.js IIFE

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,16 +1,47 @@
 'use strict';
 
 var app = app || {};
-
 const ENV = {};
 
+//these four declarations dictate which API URL will be used by our server.js file based on whether or not we are using the https protocol, which would be provided in the event we were using heroku and thus using the production API URL
 ENV.isProduction = window.location.protocol === 'https:';
 ENV.productionApiUrl = 'https://dashboard.heroku.com/apps/ms-jb-booklist';
 ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl ! ENV.developmentApiUrl;
 
-ENV.apiUrl = 
-if (ENV.isProduction){
-    ENV.apiUrl = ENV.productionApiUrl;
-} else{
-    ENV.apiUrl = ENV.developmentApiUrl;
-}
+  //the IIFE for our page initialization
+  (function(module)) {
+
+    //this function console.errors any errors detected and assigns this method to our app object which saves us time by not having to type what is executed each time we want to check for errors
+    function errorCallback(err) {
+      console.err(err);
+      module.errorView.initErrorPage(err);
+    }
+
+    //this is the constructor function for each book object from our database
+    function Book(jsonBookObj) {
+      //this uses the .keys method of the Object prototype to assign specific keys of our instance to the keys parsed from the json file, for however many keys are in the json.
+      Object.keys(jsonBookObj).forEach(key => this.[key] = jsonBookObj.[key]);
+    }
+
+    //this declaration assigns a method to each instance of our Book constructor that will compile the contents of that instance into our Handlebars template on the index.html
+    Book.prototype.toHtml = function() {
+        let template = Handlebars.compile($(`book-list-holder-placeholder`).text());
+        return template(this);
+    }
+    //this attaches a blank array to our Book constructor
+    Book.all = [];
+
+    //this .loadAll method on the Book constructor returns our Book.All array populated with sorted-by-title instances of said constructor
+    Book.loadAll = rows => Book.all = rows.sort(a,b) => b.title - a.title).map(book +. new Book(book));
+
+    //this method on the constructor listens for when /api/vi/books is requested (using the correct URL for whether or not we are using the local or cloud database) and then calls the .loadAll method
+    Book.fetchAll = callBack =>
+      $(`${ENV.apiUrl}/api/vi/books`)
+      .then(Book.loadAll)
+      .then(callBack)
+      .then(errorCallback);
+    }
+    //this final statement assigns the Book constructor as a method of our app object (the parameter for our IIFE)
+    module.Book = Book;
+  }(app)
