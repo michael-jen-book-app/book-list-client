d831d8d34cdd73de9ae69f982bfacdf0786b4f9a
Jennifer Bach <jmbach1@gmail.com>
2018-04-25 10:07:49 -0700

    added connections to servers

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -1,4 +1,16 @@
-let env = {
-    locationProtocal = 'https:';
-    
+'use strict';
+
+var app = app || {};
+
+const ENV = {};
+
+ENV.isProduction = window.location.protocol === 'https:';
+ENV.productionApiUrl = 'https://dashboard.heroku.com/apps/ms-jb-booklist';
+ENV.developmentApiUrl = 'http://localhost:3000';
+
+ENV.apiUrl = 
+if (ENV.isProduction){
+    ENV.apiUrl = ENV.productionApiUrl;
+} else{
+    ENV.apiUrl = ENV.developmentApiUrl;
 }
